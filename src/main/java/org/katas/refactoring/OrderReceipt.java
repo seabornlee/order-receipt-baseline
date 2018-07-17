485f1ca35abfcb6570bc46b88c64adad6229ee08
Seaborn Lee <seabornlee.cn@gmail.com>
2018-07-17 10:20:50 +0800

    refactor: Extract method.

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -25,14 +25,7 @@ public class OrderReceipt {
 		double totSalesTx = 0d;
 		double tot = 0d;
 		for (LineItem lineItem : order.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
-			output.append(lineItem.totalAmount());
-			output.append('\n');
+			output.append(getLineItem(lineItem));
 
 			// calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
@@ -48,6 +41,19 @@ public class OrderReceipt {
         // print total amount
 		output.append("Total Amount").append('\t').append(tot);
 		return output.toString();
+	}
+
+	private StringBuilder getLineItem(LineItem lineItem) {
+		StringBuilder output = new StringBuilder();
+		output.append(lineItem.getDescription());
+		output.append('\t');
+		output.append(lineItem.getPrice());
+		output.append('\t');
+		output.append(lineItem.getQuantity());
+		output.append('\t');
+		output.append(lineItem.totalAmount());
+		output.append('\n');
+		return output;
 	}
 
 	private String getCustomerInformation() {
