0998a84df709a88096a2f2b2def583f44f824bd7
Seaborn Lee <seabornlee.cn@gmail.com>
2018-07-17 10:08:09 +0800

    Refactor tests.

--- src/test/java/org/katas/refactoring/OrderReceiptTest.java
+++ src/test/java/org/katas/refactoring/OrderReceiptTest.java
@@ -3,38 +3,44 @@ package org.katas.refactoring;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderReceiptTest {
     @Test
     public void shouldPrintCustomerInformationOnOrder() {
+        // given
         Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>());
         OrderReceipt receipt = new OrderReceipt(order);
 
+        // when
         String output = receipt.printReceipt();
 
-        assertThat(output, containsString("Mr X"));
-        assertThat(output, containsString("Chicago, 60601"));
+        // then
+        assertThat(output).contains("Mr X");
+        assertThat(output).contains("Chicago, 60601");
     }
 
     @Test
     public void shouldPrintLineItemAndSalesTaxInformation() {
-        ArrayList<LineItem> lineItems = new ArrayList<LineItem>() {{
-            add(new LineItem("milk", 10.0, 2));
-            add(new LineItem("biscuits", 5.0, 5));
-            add(new LineItem("chocolate", 20.0, 1));
-        }};
+        // given
+        List<LineItem> lineItems = asList(
+                new LineItem("milk", 10.0, 2),
+                new LineItem("biscuits", 5.0, 5),
+                new LineItem("chocolate", 20.0, 1)
+        );
         OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
 
+        // when
         String output = receipt.printReceipt();
 
-        assertThat(output, containsString("milk\t10.0\t2\t20.0\n"));
-        assertThat(output, containsString("biscuits\t5.0\t5\t25.0\n"));
-        assertThat(output, containsString("chocolate\t20.0\t1\t20.0\n"));
-        assertThat(output, containsString("Sales Tax\t6.5"));
-        assertThat(output, containsString("Total Amount\t71.5"));
+        // then
+        assertThat(output).contains("milk\t10.0\t2\t20.0\n");
+        assertThat(output).contains("biscuits\t5.0\t5\t25.0\n");
+        assertThat(output).contains("chocolate\t20.0\t1\t20.0\n");
+        assertThat(output).contains("Sales Tax\t6.5");
+        assertThat(output).contains("Total Amount\t71.5");
     }
-
 }
