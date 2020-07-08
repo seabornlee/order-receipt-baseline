76a2f68d35081866438420407c9c4af117b51aaa
Seaborn Lee <seabornlee.cn@gmail.com>
2020-07-08 12:54:05 +0800

    xx

--- src/main/java/org/katas/refactoring/Store.java
+++ src/main/java/org/katas/refactoring/Store.java
@@ -1,27 +1,24 @@
 package org.katas.refactoring;
 
 public class Store {
+  public static final String TYPE_COMPANY_OWNED = "01";
+  private String location;
 
-    public static final String TYPE_COMPANY_OWNED = "01";
-    private String location;
+  public String getType() {
+    return type;
+  }
 
-    public String getType() {
-        return type;
-    }
+  private String type;
 
-    private String type;
+  boolean isCompanyOwned() {
+    return TYPE_COMPANY_OWNED.equals(getType()) && "China".equals(location);
+  }
 
+  public void setType(String type) {
+    this.type = type;
+  }
 
-    boolean isCompanyOwned() {
-        return TYPE_COMPANY_OWNED.equals(getType()) &&
-                                                    "China".equals(location);
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
+  public void setLocation(String location) {
+    this.location = location;
+  }
 }
