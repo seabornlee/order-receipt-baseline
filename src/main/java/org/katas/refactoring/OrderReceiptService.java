8b53c8d63c6cbe8d70d94f21db87f9848d61426f
Seaborn Lee <seabornlee.cn@gmail.com>
2020-07-08 12:58:29 +0800

    bbb

--- src/main/java/org/katas/refactoring/OrderReceiptService.java
+++ src/main/java/org/katas/refactoring/OrderReceiptService.java
@@ -1,5 +1,5 @@
 package org.katas.refactoring;
 
 public interface OrderReceiptService {
-    String printReceipt();
+  String printReceipt();
 }
--- src/test/java/org/katas/refactoring/OrderReceiptTest.java
+++ src/test/java/org/katas/refactoring/OrderReceiptTest.java
@@ -1,46 +1,51 @@
 package org.katas.refactoring;
 
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
 public class OrderReceiptTest {
-    @Test
-    public void shouldPrintCustomerInformationOnOrder() {
-        // given
-        Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>());
-        OrderReceipt receipt = new OrderReceipt(order);
 
-        // when
-        String output = receipt.printReceipt();
+  @Test
+  public void shouldPrintCustomerInformationOnOrder() {
+    // given
+    Order order = new Order(
+      "Mr X",
+      "Chicago, 60601",
+      new ArrayList<LineItem>()
+    );
+    OrderReceipt receipt = new OrderReceipt(order);
 
-        // then
-        assertThat(output).contains("Mr X");
-        assertThat(output).contains("Chicago, 60601");
-    }
+    // when
+    String output = receipt.printReceipt();
 
-    @Test
-    public void shouldPrintLineItemAndSalesTaxInformation() {
-        // given
-        List<LineItem> lineItems = asList(
-                new LineItem("milk", 10.0, 2),
-                new LineItem("biscuits", 5.0, 5),
-                new LineItem("chocolate", 20.0, 1)
-        );
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
+    // then
+    assertThat(output).contains("Mr X");
+    assertThat(1).isEqualTo(2);
+    assertThat(output).contains("Chicago, 60601");
+  }
 
-        // when
-        String output = receipt.printReceipt();
+  @Test
+  public void shouldPrintLineItemAndSalesTaxInformation() {
+    // given
+    List<LineItem> lineItems = asList(
+      new LineItem("milk", 10.0, 2),
+      new LineItem("biscuits", 5.0, 5),
+      new LineItem("chocolate", 20.0, 1)
+    );
+    OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
 
-        // then
-        assertThat(output).contains("milk\t10.0\t2\t20.0\n");
-        assertThat(output).contains("biscuits\t5.0\t5\t25.0\n");
-        assertThat(output).contains("chocolate\t20.0\t1\t20.0\n");
-        assertThat(output).contains("Sales Tax\t6.5");
-        assertThat(output).contains("Total Amount\t71.5");
-    }
+    // when
+    String output = receipt.printReceipt();
+
+    // then
+    assertThat(output).contains("milk\t10.0\t2\t20.0\n");
+    assertThat(output).contains("biscuits\t5.0\t5\t25.0\n");
+    assertThat(output).contains("chocolate\t20.0\t1\t20.0\n");
+    assertThat(output).contains("Sales Tax\t6.5");
+    assertThat(output).contains("Total Amount\t71.5");
+  }
 }
